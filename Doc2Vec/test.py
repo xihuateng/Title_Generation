10aa39dbef8af6769a78850b729d95d296ba2522
mryuan0428 <mryuan0428@126.com>
2020-08-11 23:38:59 +0800

    new

--- Doc2Vec/test.py
+++ Doc2Vec/test.py
@@ -1,7 +1,7 @@
 import os, sys
 import json
 train_data_path = sys.path[0] + "/news2016zh_train.json"
-train_data_new = sys.path[0] + "/news2016zh_train700000.json"
+train_data_new = sys.path[0] + "/news2016zh_train500000.json"
 
 i = 0
 file = open(train_data_new, 'a+', encoding="utf-8")
@@ -10,7 +10,7 @@ for line in open(train_data_path,"r", encoding="utf-8"):
     a = json.loads(line)
     file.write(json.dumps(a, ensure_ascii=False)+'\n')
     i += 1
-    if i == 700000:
+    if i == 500000:
         break
     if i % 1000 == 0:
         print(i)
