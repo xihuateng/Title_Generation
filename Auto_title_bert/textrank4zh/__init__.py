8168f185b00183a1d21af7afc9169c8a4c48a0e6
mryuan0428 <mryuan0428@126.com>
2020-08-12 10:08:03 +0800

    new

---
+++ Auto_title_bert/textrank4zh/__init__.py
@@ -0,0 +1,8 @@
+#-*- encoding:utf-8 -*-
+from __future__ import absolute_import
+from TextRank4Keyword import TextRank4Keyword
+from TextRank4Sentence import TextRank4Sentence
+import Segmentation
+import util
+
+version = '0.2'
